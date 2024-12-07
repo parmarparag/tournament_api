ccd733b313fa3c895fa587dc8ed232a4b127f62d
parmarparag <parmarparag77@gmail.com>
2024-12-07 16:06:00 +0530

    added message

--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,9 +1,8 @@
-exports.success = (res, data, message = 'Success') => {
-    res.status(200).json({ success: true, message, data });
-  };
-  
-  exports.error = (res, message = 'Error', error) => {
-    console.error(error);
-    res.status(500).json({ success: false, message });
-  };
-  
+exports.success = (res, data, message = "Success") => {
+  res.status(200).json({ success: true, message, data });
+};
+
+exports.error = (res, message = "Error", error) => {
+  console.error("Mesage:::::", error);
+  res.status(500).json({ success: false, message });
+};
